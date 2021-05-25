fafdcae21912bc2779b67e4d2c34fe26c5eef7ce
DiopWaly <waly0092@gmail.com>
2021-05-25 17:21:59 +0000

    last commit 17h21

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
+  public carousels : any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.carousels = [
+      {
+        img: "../../assets/images/images/temoignage-1.png",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        nom: "Mamadou Ndiaye",
+        status: "Ingénieur"
+      },
+      {
+        img: "../../assets/images/images/temoignage-2.png",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        nom: "Mamadou Ndiaye",
+        status: "Ingénieur"
+      },
+      {
+        img: "../../assets/images/images/temoignage-3.png",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        nom: "Mamadou Ndiaye",
+        status: "Ingénieur"
+      }
+    ];
   }
 
   left(){
